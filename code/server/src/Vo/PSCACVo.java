c75e5e42da3eb03ef5442f484f41ab4b8dae5ab2
jjc100 <jjc10000@hanmail.net>
2017-03-19 18:40:05 +0900

    modify PSCACVo.java

---
+++ code/server/src/Vo/PSCACVo.java
@@ -0,0 +1,28 @@
+package Vo;
+
+public class PSCACVo {
+
+	private String Id;
+	private String Gps;
+	private String Status;
+	
+	
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getGps() {
+		return Gps;
+	}
+	public void setGps(String Gps) {
+		Gps = Gps;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+}
