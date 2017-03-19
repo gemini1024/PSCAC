9c300c8abe2f58f5d62b57058dc7fe77e16754d6
jjc100 <jjc10000@hanmail.net>
2017-03-19 22:11:15 +0900

    change gson

---
+++ code/server/src/Vo/Message.java
@@ -0,0 +1,43 @@
+package Vo;
+
+public class Message {
+	
+	private String title;
+	private String content;
+	private String imgUrl;
+	private String link;
+	
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	
+}
