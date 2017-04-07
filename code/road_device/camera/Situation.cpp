7dc4b383062a113340401b9adf959bc89f3f18ca
JiHun-Choi <gemini10244@gmail.com>
2017-04-07 23:45:24 +0900

    add a decision based on the focus of a persion object

--- code/road_device/camera/Situation.cpp
+++ code/road_device/camera/Situation.cpp
@@ -46,10 +46,21 @@ void Situation::sendPredictedSituation(const std::vector<Rect>& foundPedestrians
     if ( !foundPedestrians.empty() ) {
        sendSignalToParentProcess( SigDef::SIG_FOUND_HUMAN );
 
+       Mat roadMat = roadImg.getMat( ACCESS_READ );
         for( auto const& r : foundPedestrians ) {
             std::cout << "Human : tl = (" << r.tl().x << "," << r.tl().y << ") , br = ("
             << r.br().x << "," << r.br().y << "), md = ("
             << ( r.br().x - r.tl().x )/2 + r.tl().x << "," << ( r.br().y - r.tl().y )/2 + r.tl().y << ")" << std::endl;
+
+
+            // Warns you if one of the top and bottom coordinates in the center of a person object has red coordinates
+            for( int i=-2; i <= 2; i++) {
+                if( roadMat.at<Vec3b>(Point( (r.br()-r.tl()).x/2 + r.tl().x, (r.br()-r.tl()).y/2 + r.tl().y + i ) )[2] == 255  ) {
+                    std::cout << " [[ Warning !! ]] Human in roadImg " << std:: endl;
+                    break;
+                }
+            }
         }
+        roadMat.release();
     }
 }
